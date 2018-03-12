dbc5701203f9ad42e07bcbf46cd3d23c72200700
Dani <bidstopper@gmail.com>
2018-03-12 10:38:09 +0100

    testing

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ webPush.setVapidDetails(
 
 webPush.setGCMAPIKey(process.env.GCM_API_KEY || null);
 
+const pushSubscription = {
+  endpoint: '< Push Subscription URL >',
+  keys: {
+    p256dh: 'BKOWm3Iz0Yg-k84Uk8a1YWaofSNAsYh3LRxVYPg6HVoivm2EH4s1Y73RQUsRN7m6rVeL9H33bxL9HCxs7d2DEvY',
+    auth: 'iyFVXcBo_cNBxK_g1wnsm_J8O_Lg0wmnwKUK7dF6Kn8'
+  }
+};
+
+let subscriptions = [];
+
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
@@ -26,27 +36,29 @@ app.get('/testing', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-  console.log(req.body);
-  res.sendStatus(201);
+  console.log(req.body, 'server body');
+  const subscription = JSON.parse(req.body);
+  if (subscriptions.some((s) => s.name === subscription.name)) {
+    res.sendStatus(201);
+  } else {
+    subscriptions.push(subscription);
+    res.sendStatus(201);
+  }
 });
 
 app.post('/sendNotification', (req, res) => {
-  setTimeout(() => {
-    console.log(req.query.endpoint, req.query.ttl);
-    webPush
-    .sendNotification({
-      endpoint: req.query.endpoint,
-      TTL: req.query.ttl
-    })
-    .then(() => {
-      console.log("push notification has been sent");
-      res.sendStatus(201);
-    })
-    .catch((error) => {
-      res.sendStatus(500);
-      console.log(error);
-    });
-  }, req.query.delay * 1000);
+  console.log(req.body, 'body');
+  const subscription = JSON.parse(req.body);
+  webPush
+  .sendNotification(Object.assign(pushSubscription, {endpoint: subscription.endpoint}), 'working')
+  .then(() => {
+    console.log("push notification has been sent");
+    res.sendStatus(201);
+  })
+  .catch((error) => {
+    res.sendStatus(500);
+    console.log(error);
+  });;
 });
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
