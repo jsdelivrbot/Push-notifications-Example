6d1e2403511dd9741f01118dcbb898616404053a
Dani <bidstopper@gmail.com>
2018-03-12 16:41:46 +0100

    updated service worker

--- public/service-worker-1.0.js
+++ public/service-worker-1.0.js
@@ -1,7 +1,21 @@
-self.addEventListener('push', function(event) {
+self.addEventListener('push', (event) => {
   let data = event.data.json();
 
   event.waitUntil(
     self.registration.showNotification(data.title, data.options)
   );
 });
+
+self.addEventListener('notificationclick', (event) => {
+  console.log(event);
+  var messageId = event.notification.data;
+
+  event.notification.close();
+
+  if (event.action === 'accept') {
+    clients.openWindow("/accept");
+  }
+  else {
+    clients.openWindow("/dismiss");
+  }
+}, false);
